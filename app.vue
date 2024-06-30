3c3123b7a11603693c2229a05285ec802f474a42
Fakhri Rossi <rossifakhri@gmail.com>
2024-06-30 17:47:19 +0700

    added modules: nuxtimage and icon

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="bg-blue-500 flex justify-center items-center h-11">
+    <h1 class="text-red-200"> <Icon name="ph:address-book-light" /> Haloo</h1>
   </div>
+  <p class="text-red-600 text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam provident voluptatum adipisci enim! Nam eligendi repellat, libero fugit non inventore perspiciatis totam porro, neque corrupti animi, nulla vitae! Culpa, ullam.</p>
+  <!-- <NuxtImg src="./assets/img/2813278658.jpg" /> -->
+  <img src="./assets/img/2813278658.jpg" alt="">
+  <NuxtImg src="/2813278658.jpg" />
 </template>
